4d84d2c34dc9482bdae7f2bf8f3a3b8bd80bc7cb
EliasSimoes <eliasunipdesign@gmail.com>
2016-07-05 20:21:21 -0300

    Jeito tradicional de criar arrays bi-dimensionais.

---
+++ ArrayBidimensionalFormaTradicional.java
@@ -0,0 +1,41 @@
+package cap11;
+
+public class ArrayBidimensionalFormaTradicional {
+
+	public static void main(String[] args) {
+		// criando array bidimensionais (com arrays anônimos de tamanhos diferentes) pelo modo tradicional
+		
+		
+		int numeros [][] = new int[3][];
+		
+		int primeiro [] = new int [3];
+		int segundo  [] = new int [4];
+		int terceiro [] = new int [2];
+		
+		primeiro[0] = 0;
+		primeiro[1] = 1;
+		primeiro[2] = 1;
+
+		segundo[0] = 0;
+		segundo[1] = 1;
+		segundo[2] = 1;
+		segundo[3] = 2;
+
+		terceiro[0] = 21;
+		terceiro[1] = 34;
+
+		numeros[0] = primeiro;
+		numeros[1] = segundo;
+		numeros[2] = terceiro;
+		
+		for (int i = 0; i < numeros.length; i++){
+			System.out.println("---------------------");
+			for (int j = 0; j < numeros[i].length; j++){
+				System.out.println(numeros[i][j]);
+			}
+		}
+
+
+	}
+
+}
