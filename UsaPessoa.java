392548f2981b560a12f3daa815cdea13b48464fa
EliasSimoes <eliasunipdesign@gmail.com>
2016-07-05 04:25:56 -0300

    Update UsaPessoa.java

--- UsaPessoa.java
+++ UsaPessoa.java
@@ -5,13 +5,13 @@ public class UsaPessoa {
 
 		Pessoa pessoas[] = new Pessoa[2]; // declara a array pessoa
 
-		pessoas[0] = new Pessoa(); // cria a instancia para a posição ZERO
+		pessoas[0] = new Pessoa(); // cria a instancia para a posiÃ§Ã£o ZERO
 		pessoas[0].setNome("Maria");
 		pessoas[0].setPeso(55);
 		pessoas[0].setAltura(1.68);
 
-		pessoas[1] = new Pessoa(); // cria a instancia para a posição ZERO
-		pessoas[1].setNome("José");
+		pessoas[1] = new Pessoa(); // cria a instancia para a posiÃ§Ã£o UM
+		pessoas[1].setNome("JosÃ©");
 		pessoas[1].setPeso(60);
 		pessoas[1].setAltura(1.77);
 
